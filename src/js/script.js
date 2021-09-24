698895c339de8b6c879eee5b0ac520e1f2ed0caf
Vladimir <govorov2001gv@gmail.com>
2021-09-24 16:08:04 +0300

    test main-commands JS

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+/* a = 15; */
+/* let number = 10;
+
+number = 16;
+console.log(number);
+
+const obj = {
+    name: "vladimir",
+    age: 25,
+    isMerried: false
+};
+console.log(obj["name"]);
+console.log(obj.name);
+
+const arr = [14, 15, 1, 29, 'str'];
+console.log(arr[4]); */
+
+/* const result = confirm("Вам есть 18 лет?");
+console.log(result); */
+
+/* const result = prompt("Сколько Вам лет?", "");
+console.log(result); */
+
+/* let category = 'toys',
+    numberOfToy = 5;
+console.log(`https://someurl/${category}/${numberOfToy}`); */
